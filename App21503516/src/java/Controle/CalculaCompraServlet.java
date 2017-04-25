e212a9e626549df31c7afc33ea32a38f0c7635a1
Fabio <>
2017-04-24 22:28:10 -0300

    Av MM

--- App21503516/src/java/Controle/CalculaCompraServlet.java
+++ App21503516/src/java/Controle/CalculaCompraServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import jdk.nashorn.internal.ir.RuntimeNode;
 
 /**
  *
@@ -18,6 +19,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class CalculaCompraServlet extends HttpServlet {
 
+    
+    
+    
     
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -34,7 +38,41 @@ public class CalculaCompraServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         
         try (PrintWriter out = response.getWriter()) {
+            double p1 = 22.76;
+            double p2 = 18.00;
+            double p3 = 25.00;
+            double p4 = 8.76;
+            double p5 = 17.54;
+            double p6 = 35.00;
+            int totalP1 = 0;
+            int totalP2 = 0;
+            double imp = 8 / 100;
+            int Cod1 = Integer.parseInt(request.getParameter("tfCodProduto1"));
+            int Cod2 = Integer.parseInt(request.getParameter("tfQtdProduto2"));
+            int Qtd1 = Integer.parseInt(request.getParameter("tfQtdProduto1"));
+            int Qtd2 = Integer.parseInt(request.getParameter("tfQtdProduto2"));
             
+            switch (Cod1){
+                case 1 : Cod1 = (int) p1;
+                break;
+                case 2 : Cod1 = (int) p2;
+                break;
+                case 3 : Cod1 = (int) p3;
+                break;
+                case 4 : Cod1 = (int) p4;
+                break;
+                case 5 : Cod1 = (int) p5;
+                break;
+                case 6 : Cod1 = (int) p6;
+                break;
+            }
+            
+            totalP1 = (int) (p1 * Qtd1);
+            totalP2 = (int) (p1 * Qtd2);
+               
+               
+                            
+                
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -44,8 +82,9 @@ public class CalculaCompraServlet extends HttpServlet {
             out.println("<body>");
             out.println("<h1>Total das compras</h1>");
             out.println("<tr>");
-            out.println("<td>Produto 1: </td><br>");
-            out.println("<td>Produto 2: </td><br>");
+            out.println("<td>Produto 1:" + totalP1 + "</td><br>");
+            out.println("<td>Produto 2:" + totalP2 + " </td><br>");
+            out.println("<td>Imposto:" + imp + " </td><br>");
             out.println("</tr>");
             out.println("<br><input type='submit' value='voltar' />");
             out.println("</body>");
